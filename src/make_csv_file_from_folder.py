d7e9c73a450f7990545a6d28d32be0f6d02ff0e0
matvey83 <matvey83@be02edf8-907f-434d-9d52-e0ffb5e9bc3f>
2010-12-23 11:52:09 +0000

    Updated to save the URLs properly to the CSV file.
    git-svn-id: https://softchord.svn.sourceforge.net/svnroot/softchord@134 be02edf8-907f-434d-9d52-e0ffb5e9bc3f

--- src/make_csv_file_from_folder.py
+++ src/make_csv_file_from_folder.py
@@ -4,12 +4,15 @@ import sys, os
 import glob
 import csv
 
+import urllib
+
+
+
 
 folder_path = sys.argv[1]
 output_file = sys.argv[2]
 
 print 'Folder path:', folder_path
-print 'Output CSV file:', output_file
 
 print ''
 
@@ -17,7 +20,7 @@ print ''
 fh = open(output_file, 'w')
 writer = csv.writer(fh)
 
-header = ["name","path"]
+header = ["Song name","Download link"]
 writer.writerow(header)
 
 bu_dir = os.getcwd()
@@ -26,14 +29,18 @@ os.chdir(folder_path)
 
 for filename in glob.glob("*.doc"):
     print '  ', filename
-    name = filename[:-4] # strip ".doc"
-    row = [name, filename]
-    print 'ROW:', row
+    name = filename[:-4] #.decode('utf-8') # strip ".doc"
+    url = "http://churchconcord.org/songs/" + urllib.quote(filename)
+    link_name = "Download"
+    value = '<a href="%s">%s</a>' % (url, link_name)
+    row = [name, value]
+    #print 'ROW:', name.encode('utf-8'), filename.encode('utf-8')
     writer.writerow(row)
 
 os.chdir(bu_dir)
 
 fh.close()
 
+print 'Output CSV file:', output_file
 
 
