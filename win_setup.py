ab5d239382dd32a8c866f47040d89bf48c8a8b81
matvey83 <matvey83@be02edf8-907f-434d-9d52-e0ffb5e9bc3f>
2010-12-31 23:52:09 +0000

    Minor changes
    git-svn-id: https://softchord.svn.sourceforge.net/svnroot/softchord@187 be02edf8-907f-434d-9d52-e0ffb5e9bc3f

--- win_setup.py
+++ win_setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup  
 import py2exe  
 
-setup(name="softchordeditor_gui",  
-      version="0.1",  
+setup(name="softChord Editor",  
+      version="",  
       author="Matvey Adzhigirey",  
       author_email="",  
       url="",  
       license="GNU General Public License (GPL)",  
       scripts=["src/softchordeditor_gui.py"],
-      data_files=["src/softchordeditor.ui", "src/softchordeditor_chord_dialog.ui", "src/song_database.sqlite"],
+      data_files=["src/softchordeditor.ui", "src/softchordeditor_chord_dialog.ui", "src/softchordeditor_pdf_dialog.ui", "src/zvuki_neba.songbook"],
       windows=[{"script": "src/softchordeditor_gui.py"}],  
       options={"py2exe": {"skip_archive": False, "includes": ["sip"]}}
       #options={"py2exe":{"includes":["sip"]}, "packages":["PyQt4"], "bundle_files":1, "compressed":False, "xref":True}
