2f01bfad256704916b008d555ca06b0b86d97f0f
matvey83 <matvey83@be02edf8-907f-434d-9d52-e0ffb5e9bc3f>
2012-06-18 07:16:50 +0000

    Now functional
    git-svn-id: https://softchord.svn.sourceforge.net/svnroot/softchord@376 be02edf8-907f-434d-9d52-e0ffb5e9bc3f

--- softchordweb/static/softchordweb.js
+++ softchordweb/static/softchordweb.js
@@ -1,5 +1,5 @@
 function SongTableCtrl($scope, $http) {
-  $scope.current_song = {title:"NONE"};
+  $scope.current_song = {title:""};
   $scope.current_html = "";
   
   $scope.songs = [];
@@ -8,10 +8,6 @@ function SongTableCtrl($scope, $http) {
       $scope.songs = data;
   });
 
-  $scope.addTodo = function() {
-    $scope.songs.push({title:$scope.todoText, number:0, id:0});
-    $scope.todoText = '';
-  };
   
   $scope.loadSong = function(number) {
     var curr_song = 0;
@@ -22,23 +18,13 @@ function SongTableCtrl($scope, $http) {
     
     $scope.current_song = curr_song;
     if($scope.current_song) {  
-        $http.get('/songs/%i' % curr_song.number).success(function(data) {
+        $http.get('/songs/' + curr_song.number).success(function(data) {
             $scope.current_html = data;
-            //$scope.current_html = curr_song.text;
         });
     }
     
   };
 
  
-  $scope.archive = function() {
-    var oldSong = $scope.songs;
-    $scope.songs = [];
-    angular.forEach(oldSong, function(todo) {
-      if (!todo.done) $scope.songs.push(todo);
-    });
-  };
-
-
 }
 
