8043c85442a356308f50d456bcbb666da578461d
Matvey Adzhigirey <matvey.adzhigirey@schrodinger.com>
2024-03-09 23:52:33 -0500

    Remove `softchordapp` (which was never completed)

--- softchordapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
-
-urlpatterns = patterns('',
-		(r'^services/$', 'songs.views.service'),
-		(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-			{'document_root': settings.STATIC}),
-)
